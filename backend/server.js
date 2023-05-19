d4d854fbaf4418d01e4cd75e1fca1a882476b71b
Andrii Skokovskyi <a.softwarework@gmail.com>
2023-05-19 16:07:44 +0300

    feat: Add custom middleware

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 import express from "express";
 import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes.js'
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config()
+
 const port = process.env.PORT || 3500;
 const app = express();
 
-app.get( '/', ( req, res ) => res.send( 'Server is ready' ) )
+app.use( '/api/users', userRoutes );
 
-app.listen( port, () => console.log( `'Server is running on port: ${ port }` ) )
+app.get( '/', ( req, res ) => res.send( 'Server is ready' ) );
+
+app.use( notFound );
+app.use( errorHandler );
+
+app.listen( port, () => console.log( `'Server is running on port: ${ port }` ) );
 
