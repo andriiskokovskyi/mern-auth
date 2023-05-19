051625b2304b45c5cd54485092da69daf6dcc2a7
Andrii Skokovskyi <a.softwarework@gmail.com>
2023-05-19 20:32:20 +0300

    feat: Add create user with hashed password

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,9 +1,35 @@
 import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
 
 // POST /api/users/
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Register user'});
-});
+    const {name, email, password} = req.body;
+    // console.log(name, email, password);
+    const userExist = await User.findOne({email});
+
+    if (userExist) {
+        res.status(400);
+        throw new Error('User is already exist');
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+}
+);
 
 // POST /api/users/auth
 const authUser = asyncHandler(async (req, res) => {
